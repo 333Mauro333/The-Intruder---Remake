5acd587194c822b0ab652b768e3c535ba20c462c
333Mauro333 <maurotv50@gmail.com>
2024-12-15 01:03:31 -0300

    Agregado de salto y agachada
    El jugador puede saltar y agacharse. Los valores de velocidad y altura pueden alterarse.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,85 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Values")]
-    [SerializeField] float speed;
+    [SerializeField] float normalSpeed;
+    [Space(8)]
+    [SerializeField] float crouchSpeed;
+    [SerializeField] float crouchHeight;
+    [Space(8)]
+    [SerializeField] float gravity;
+    [SerializeField] float jumpForce;
 
-    CharacterController cc;
+    [Header("References")]
+    [SerializeField] Camera camera;
+
+	float normalHeight;
+	float fallVelocity;
+    float currentSpeed;
+
+	CharacterController cc;
+
 
 
 	void Awake()
 	{
+		fallVelocity = 0.0f;
+        currentSpeed = normalSpeed;
+
         cc = GetComponent<CharacterController>();
+
+		normalHeight = cc.height;
 	}
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        ApplyGravitySystem();
 
-        cc.Move(transform.right * horizontalInput * speed * Time.deltaTime);
-        cc.Move(transform.forward * verticalInput * speed * Time.deltaTime);
-    }
+        ControlJumpInput();
+		ControlCrouchInput();
+		ControlMovementInput();
+	}
+
+
+    void ApplyGravitySystem()
+    {
+        // Limita la velocidad de caída cuando el jugador está sobre el piso.
+		if (cc.isGrounded && fallVelocity < 0.0f)
+		{
+			fallVelocity = -1.0f;
+		}
+
+		// Aplica gravedad al jugador.
+		fallVelocity += gravity * Time.deltaTime;
+		cc.Move(transform.up * fallVelocity * Time.deltaTime);
+	}
+
+    void ControlJumpInput()
+    {
+		if (Input.GetButtonDown("Jump") && cc.isGrounded)
+		{
+			fallVelocity = Mathf.Sqrt(jumpForce * -1f * gravity);
+		}
+	}
+    void ControlCrouchInput()
+    {
+		if (Input.GetButtonDown("Crouch"))
+		{
+			cc.height = crouchHeight;
+			currentSpeed = crouchSpeed;
+		}
+
+		if (Input.GetButtonUp("Crouch"))
+		{
+			cc.height = normalHeight;
+			currentSpeed = normalSpeed;
+		}
+	}
+	void ControlMovementInput()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+
+		cc.Move(transform.right * horizontalInput * currentSpeed * Time.deltaTime +
+		transform.forward * verticalInput * currentSpeed * Time.deltaTime);
+	}
 }
