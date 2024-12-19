3e5e32c7e66ea71fa8255f8e581f190800be358c
333Mauro333 <maurotv50@gmail.com>
2024-12-18 23:47:19 -0300

    Agregado de rotación de la torreta
    La torreta detecta al jugador cuando este la ve. Rota con una velocidad definida, y cuando el jugador se esconde, deja de seguirlo.

--- Assets/Scripts/Enemies/Enemy Turret/EnemyTurretSearch.cs
+++ Assets/Scripts/Enemies/Enemy Turret/EnemyTurretSearch.cs
@@ -3,13 +3,66 @@ using UnityEngine;
 
 public class EnemyTurretSearch : MonoBehaviour
 {
+    [Header("Values")]
+    [SerializeField] float speed;
+    [SerializeField] float distanceToDetect;
+
     [Header("References")]
     [SerializeField] Transform target;
+    [SerializeField] Transform[] rayOriginPoints;
 
 
+    bool targetWasDetected;
+
+
+
+    void Awake()
+    {
+        targetWasDetected = false;
+    }
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     void Update()
     {
-        transform.LookAt(target);
+		targetWasDetected = SearchPlayer();
+
+		if (targetWasDetected)
+        {
+            RotateToTarget();
+		}
     }
+
+
+
+    void RotateToTarget()
+    {
+		Vector3 direction = target.position - transform.position;
+		Quaternion desiredRotation = Quaternion.LookRotation(direction);
+
+		transform.rotation = Quaternion.RotateTowards(transform.rotation,
+													  desiredRotation,
+													  speed * Time.deltaTime);
+	}
+	bool SearchPlayer()
+	{
+		for (int i = 0; i < rayOriginPoints.Length; i++)
+		{
+			RaycastHit ray;
+			Vector3 direction = target.position - rayOriginPoints[i].position;
+
+			if (Physics.Raycast(rayOriginPoints[i].position, direction, out ray, distanceToDetect))
+			{
+				if (ray.collider.tag == "Player")
+				{
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
 }
